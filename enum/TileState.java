ada300f006aba3295594d9cf724a8431405f34a8
anisaalhaqi <anisaalhaqi@gmail.com>
2025-12-02 19:36:14 +0700

    feat: initial commit

---
+++ enum/TileState.java
@@ -0,0 +1,32 @@
+public enum TileState {
+    WALL("X"),
+    CUTTING_STATION("C"),
+    COOKING_STATION("R"),
+    ASSEMBLY_STATION("A"),
+    SERVING_COUNTER("S"),
+    WASHING_STATION("W"),
+    INGREDIENT_STORAGE("I"),
+    PLATE_STORAGE("P"),
+    TRASH_STATION("T"),
+    WALKABLE("."),
+    SPAWN("V");
+
+    private final String symbol;
+
+    TileState(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public static String fromSymbol(String symbol) {
+        for(TileState state: TileState.values()) {
+            if(state.symbol.equals(symbol)) {
+                return state.getName();
+            }
+        }
+        return null;
+    }
+
+    public String getName() {
+        return this.name();
+    }
+}
