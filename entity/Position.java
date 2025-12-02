366c978ccf2190e43959ef56c1b18de5d27c3b6a
anisaalhaqi <anisaalhaqi@gmail.com>
2025-12-02 18:10:50 +0700

    feat: initial commit of Position.java file

---
+++ entity/Position.java
@@ -0,0 +1,23 @@
+public class Position {
+    private final int x;
+    private final int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public static Position getAdjacent(Position current, Direction direction) {
+        int newX = current.getX() + direction.getX();
+        int newY = current.getY() + direction.getY();
+
+        return new Position(newX, newY);
+    }
+}
