71e687d18e905385207e7375fa17d89f65490912
anisaalhaqi <anisaalhaqi@gmail.com>
2025-12-02 19:25:37 +0700

    chore: temporarily comment out map validation for GUI testing

---
+++ entity/PizzaMap.java
@@ -0,0 +1,35 @@
+public class PizzaMap extends MapType {
+    private static final String[] PIZZA_LAYOUT = {
+            "XATACAAACAAAXX",
+            "X...........XX",
+            "X.....A.V...SX",
+            "X...........SX",
+            "XWWAIAIAIAIAPX",
+            "X............X",
+            "XXXX..A...XXXX",
+            "XR...V......RX",
+            "XXXX......XXXX",
+            "XXXXAAIAAAXXXX",
+    };
+
+    @Override
+    protected String[] getLayoutData() {
+        return PIZZA_LAYOUT;
+    }
+
+    @Override
+    protected String getMapName() {
+        return "Pizza Map";
+    }
+
+    @Override
+    protected void initialLayout() {
+        for(int i = 0; i < getHeight(); i++) {
+            String rowLayout = PIZZA_LAYOUT[i];
+            for(int j = 0; j < getWidth(); j++) {
+                String symbol = String.valueOf(rowLayout.charAt(j));
+                tiles[i][j] = new Tile(i, j, symbol);
+            }
+        }
+    }
+}
