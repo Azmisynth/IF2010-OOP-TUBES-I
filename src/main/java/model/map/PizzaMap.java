eab04a03c49433fa2b03ae9c57b0bc21902f55ac
anisaalhaqi <anisaalhaqi@gmail.com>
2025-12-05 14:16:52 +0700

    chore: create list of chef position to easily initiate chef object based on the spawn position

--- src/main/java/model/map/PizzaMap.java
+++ src/main/java/model/map/PizzaMap.java
@@ -1,6 +1,11 @@
 package main.java.model.map;
 
+import main.java.model.chef.Position;
+import java.util.List;
+import java.util.ArrayList;
+
 public class PizzaMap extends MapType {
+    private List<Position> chefPosition = new ArrayList<>();
     private static final String[] PIZZA_LAYOUT = {
             "XATACAAACAAAXX",
             "X...........XX",
@@ -13,6 +18,19 @@ public class PizzaMap extends MapType {
             "XXXX......XXXX",
             "XXXXAAIAAAXXXX",
     };
+
+    public List<Position> getChefPositions() {
+        for(int i = 0; i < getHeight(); i++) {
+            String rowLayout = PIZZA_LAYOUT[i];
+            for(int j = 0; j < getWidth(); j++) {
+                String symbol = String.valueOf(rowLayout.charAt(j));
+                if(symbol.equals("V")) {
+                    chefPosition.add(new Position(j, i));
+                }
+            }
+        }
+        return java.util.Collections.unmodifiableList(this.chefPosition);
+    }
 
     @Override
     protected String[] getLayoutData() {
@@ -30,6 +48,9 @@ public class PizzaMap extends MapType {
             String rowLayout = PIZZA_LAYOUT[i];
             for(int j = 0; j < getWidth(); j++) {
                 String symbol = String.valueOf(rowLayout.charAt(j));
+                if(symbol.equals("V")) {
+                    chefPosition.add(new Position(i, j));
+                }
                 tiles[i][j] = new Tile(i, j, symbol);
             }
         }
